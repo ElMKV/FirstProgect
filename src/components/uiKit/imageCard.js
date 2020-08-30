5e60727b5897a0fb9bd16759155254afada739e3
ELMKV <ilya.makoveev021199@gmail.com>
2020-08-30 13:48:49 +0500

    Добавил hairline

--- src/components/uiKit/imageCard.js
+++ src/components/uiKit/imageCard.js
@@ -1,42 +1,44 @@
 import React from 'react'
 import { Text, StyleSheet, View, Image } from 'react-native'
-import { w, h} from '../../../constant'
+import { w, h } from '../../../constant'
 
 
-const ImageCard = ({data}) => {
-    const { h1, cover, container,yearStyle, containerNameBalance, containerNameBalanceStyle} = styles
-    const { picture, first, age, balance} = data
+const ImageCard = ({ data }) => {
+    const { h1, cover, container, yearStyle, containerNameBalance, containerNameBalanceStyle } = styles
+    const { picture, first, age, balance } = data
     return (
         <View style={container}>
-                <Image style={cover} source = {{uri: picture}} />
-                <View style = { containerNameBalance}>
-                    <Text style = {h1}> {first}</Text>
-                    <Text style = {containerNameBalanceStyle}>{ balance }</Text> 
-                </View>
-                
-                <Text style={yearStyle}>{age}</Text>  
-                            
-       </View>
-       
+            <Image style={cover} source={{ uri: picture }} />
+            <View style={containerNameBalance}>
+                <Text style={h1}> {first}</Text>
+                <Text style={containerNameBalanceStyle}>{balance}</Text>
+            </View>
+
+            <Text style={yearStyle}>{age}</Text>
+
+        </View>
+
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         flexDirection: 'row',
-        height: h/8,
+        height: h / 8,
         paddingVertical: 10,
-        backgroundColor: 'white'
-        
+        backgroundColor: 'white',
+        borderBottomWidth: StyleSheet.hairlineWidth
+
+
     },
     h1: {
         fontFamily: 'AvenirNext-DemiBold',
         fontSize: 18,
         flex: 2
-        
-        
-        
+
+
+
     },
 
     yearStyle: {
@@ -44,38 +46,38 @@ const styles = StyleSheet.create({
         color: 'black',
         textAlign: 'center',
         flex: 1
-        
-        
-        
+
+
+
 
     },
     cover: {
         borderRadius: 50,
         justifyContent: 'center',
-        flex:1,
+        flex: 1,
         marginLeft: 10
-        
-        
+
+
 
 
     },
-    containerNameBalance:{
+    containerNameBalance: {
         flexDirection: 'column',
         flex: 3,
         marginLeft: 5,
-        
+
 
 
 
     },
-    containerNameBalanceStyle:{
-        textAlignVertical:'top',
+    containerNameBalanceStyle: {
+        textAlignVertical: 'top',
         fontSize: 16,
         color: '#696969',
     }
 
-    
-    
+
+
 })
 
-export  {ImageCard}
+export { ImageCard }
